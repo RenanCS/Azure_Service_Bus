be153ce6634d0303576bc991ff6d0daea589a08a
renan <renan@interprocess.com.br>
2022-04-20 11:47:54 -0300

    feat: add project message entities

---
+++ SimpleBrokeredMessaging.MessageEntities/PizzaOrder.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleBrokeredMessaging.MessageEntities
+{
+    public class PizzaOrder
+    {
+        public PizzaOrder(string customerName, string type, string size, int id? = 0)
+        {
+            Id = id;
+            CustomerName = customerName;
+            Type = type;
+            Size = size;
+        }
+
+        public int? Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Type { get; set; }
+        public string Size { get; set; }
+    }
+}
